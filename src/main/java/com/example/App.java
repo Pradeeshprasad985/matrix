68a95424495a9a4363357616ee573ce420c25275
pradeesh prasad <pradeeshprassad985@gmail.com>
2026-01-09 10:21:03 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub Actions!");
+        System.out.println("Maven build is working successfully 🚀");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
